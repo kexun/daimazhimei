8e3ee454b9e2879673138a9b5833aeb8f206daf5
kexun <290464784@qq.com>
2016-06-11 23:58:02 +0800

    quicksort

--- src/com/daimazhimei/Day01.java
+++ src/com/daimazhimei/Day01.java
@@ -10,16 +10,22 @@ public class Day01 {
 	public static void main(String[] args) {
 
 		Day01 d = new Day01();
-		int[] array = new int[20];
+		int[] array = new int[8];
 		
 		for (int i = 0; i<array.length; i++) {
 			array[i] = (int)(1+Math.random()*(1000-1+1));
 		}
 		
-		d.quickSort(array, 0, array.length-1);
+		d.quickSort2(array, 0, array.length-1);
 		d.print(array);
 	}
 	
+	/**
+	 * 实现方式一
+	 * @param array
+	 * @param left
+	 * @param right
+	 */
 	public void quickSort(int[] array, int left, int right) {
 		
 		int length = array.length;
@@ -62,6 +68,47 @@ public class Day01 {
 	public void print(int[] array) {
 		for (int a : array)
 			System.out.print(a+",");
+	}
+	
+	/**
+	 * 实现方式二
+	 * @param array
+	 * @param left
+	 * @param right
+	 */
+	public void quickSort2(int[] array, int left, int right) {
+		
+		if (left > right) {
+			return;
+		}
+		
+		int m = left;
+		
+		for (int i=left+1; i<=right; i++) {
+			if (array[i] < array[left]) {
+				swap(array, ++m, i);
+			}
+		}
+		
+		swap(array, left, m);
+		quickSort2(array, left, m-1);
+		quickSort2(array, m+1, right);
+		
+	}
+	
+	/**
+	 * 交换值
+	 * @param array
+	 * @param l
+	 * @param r
+	 */
+	public void swap(int[] array, int l, int r) {
+		if (array.length == 0)
+			return;
+		
+		int temp = array[r];
+		array[r] = array[l];
+		array[l] = temp;
 	}
 	
 }

d09ba9815f3e630387bab7cffd62c6db5f88b3af
kexun <290464784@qq.com>
2016-06-12 17:01:43 +0800

    随机数组生成器， 用于测试

--- src/com/daimazhimei/Day02.java
+++ src/com/daimazhimei/Day02.java
@@ -1,6 +1,10 @@
 package com.daimazhimei;
 
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -38,16 +42,74 @@ public class Day02 {
 		return -1;
 	}
 	
+	/**
+	 * 用于测试二分查找中，数组过大，求中间值溢出问题。
+	 * @param low
+	 * @param high
+	 * @return
+	 */
+	public int calculateMidPoind(int low, int high) {
+		int mid = (low + high) >>> 1;
+		System.out.println((low + high));
+		return mid;
+	}
+	
+	/**
+	 * 用于测试二分查找中，数组过大，求中间值溢出问题。
+	 * @param low
+	 * @param high
+	 * @return
+	 */
+	public int calculateMidPoind2(int low, int high) {
+		int mid = (low + high) /2;
+		return mid;
+	}
+	
+	public int[] generateRandomSortedArray(int maxArraySize, int maxValue) {
+		
+		int arraySize = 1 + new Random().nextInt(maxArraySize);
+		int[] array = new int[arraySize];
+		
+		for (int i = 0; i < arraySize; i++) {
+			array[i] = new Random().nextInt(maxValue);
+		}
+		
+		Arrays.sort(array);
+		for (int a : array) {
+			System.out.print(a+",");
+		}
+		
+		return array;
+	}
+	
 	@Test
 	public void smokeTest() {
 		Day02 d = new Day02();
-		int[] array = new int[50];
+		int[] array = {
+			Integer.MIN_VALUE,-23,-5,-1,0,45,67,89
+		};
 		
-		for (int i = 0; i < 50; i++) {
-			array[i] = i;
-		}
-		System.out.println(array[d.binarySearch(array, 10)]);
-		Assert.assertEquals(9, d.binarySearch(array, 10));
+		
+		int v1 = d.calculateMidPoind2(Integer.MAX_VALUE-2, Integer.MAX_VALUE-1);
+		int v2 = d.calculateMidPoind(Integer.MAX_VALUE-2, Integer.MAX_VALUE-1);
+		int v3 = d.calculateMidPoind(1200000000, 1300000000);
+		
+		System.out.println(v1);
+		System.out.println(v2);
+		System.out.println(v3);
+		
+		int v4 = d.binarySearch(array, -23);
+		System.out.println(v4);
+		int v5 = d.binarySearch(array, 89);
+		System.out.println(v5);
+		int v6 = d.binarySearch(array, Integer.MIN_VALUE);
+		System.out.println(v6);
+		
+		System.out.println("/////////////////////////");
+		int[] array2 = d.generateRandomSortedArray(40, 90);
+		int v7 = d.binarySearch(array2, 60);
+		System.out.println(v7);
+		
 	}
 	
 }
